1599215a88b97724fcfbf466b495ab1730b979b1
dlnioanna <dln.ioanna@gmail.com>
2021-04-03 16:41:37 +0300

    editing layouts added Navcontroller

--- app/src/main/java/unipi/protal/countriesteach/MainActivity.java
+++ app/src/main/java/unipi/protal/countriesteach/MainActivity.java
@@ -1,14 +1,18 @@
 package unipi.protal.countriesteach;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController);
     }
 }
